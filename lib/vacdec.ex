59fb8f680045f17268fddad3a763a86057f0121a
Udo Schneider <Udo.Schneider@homeaddress.de>
2021-11-04 11:20:53 +0100

    Initial commit

---
+++ lib/vacdec.ex
@@ -0,0 +1,18 @@
+defmodule Vacdec do
+  @moduledoc """
+  Documentation for `Vacdec`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Vacdec.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/vacdec_test.exs
@@ -0,0 +1,8 @@
+defmodule VacdecTest do
+  use ExUnit.Case
+  doctest Vacdec
+
+  test "greets the world" do
+    assert Vacdec.hello() == :world
+  end
+end
